9639df48b768af6e593568ecaf226adbf50a1973
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2020-03-29 22:31:46 +0200

    add current_time

--- app/controllers/rails_ping/ping_controller.rb
+++ app/controllers/rails_ping/ping_controller.rb
@@ -3,11 +3,12 @@
 class RailsPing::PingController < ActionController::Base
   def ping
     map = {
-      'hostname' => Socket.gethostname,
-      'revision' => ENV['HEROKU_SLUG_COMMIT'],
-      'version' => ENV['HEROKU_RELEASE_VERSION'],
-      'created_at' => ENV['HEROKU_RELEASE_CREATED_AT'],
-      'app' => ENV['HEROKU_APP_NAME']
+      hostname: Socket.gethostname,
+      revision: ENV['HEROKU_SLUG_COMMIT'],
+      version: ENV['HEROKU_RELEASE_VERSION'],
+      created_at: ENV['HEROKU_RELEASE_CREATED_AT'],
+      application: ENV['HEROKU_APP_NAME'],
+      current_time: Time.current
     }
     render json: map
   end
