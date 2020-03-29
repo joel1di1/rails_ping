50e5a8d3ded825ad87f1c77180cfdaedb2adbc11
Benoit Lafontaine <benoit.lafontaine@doctolib.com>
2020-03-29 22:30:36 +0200

    require rails engine

--- lib/rails_ping.rb
+++ lib/rails_ping.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+require 'rails'
 require 'rails_ping/version'
 
-class RailsPing::Error < StandardError; end
+module RailsPing
+  # Our host application root path
+  # We set this when the engine is initialized
+  mattr_accessor :app_root
+
+  # Yield self on setup for nice config blocks
+  def self.setup
+    yield self
+  end
+end
+
+# Require our engine
+require 'rails_ping/engine'
